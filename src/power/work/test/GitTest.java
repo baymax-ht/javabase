1f28c70dd797884940c666606a73fc98b01783db
baymax <2356235991@qq.com>
2020-10-09 22:38:57 +0800

    first commit add project to git

---
+++ src/power/work/test/GitTest.java
@@ -0,0 +1,10 @@
+package power.work.test;
+
+public class GitTest {
+    public static void main(String[]args){
+        System.out.println("sd");
+        System.out.println("sd");
+        System.out.println("sd");
+        System.out.println("sd");
+    }
+}
